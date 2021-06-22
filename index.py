a92479898f220b0c7343fae15c1a116fc08ba896
Willian Cavalcanti Coelho <minegt1500@gmail.com>
2021-06-22 08:14:39 -0300

    Bot agora pode ser utilizado em qualquer servidor

--- index.py
+++ index.py
@@ -15,17 +15,24 @@ bot.remove_command("help")
 @bot.event
 async def on_ready():
     print("bot online")
-#hi
+
 @bot.event
 @commands.guild_only()
 async def on_message(ctx):
-    if ctx.guild.id != 784050272263471145:
+    if ctx.author.bot == True:
         return
-    if ctx.author == bot.user:
-        return
+    try:
+        arquivo = pd.read_csv(f'arquivo/{ctx.guild.id}.csv')
+    except:
+        dado_inserir = pd.DataFrame(columns=['ChatID', 'ChatName', 'CountMessage'])
+        dado_inserir.set_index("ChatID", inplace=True)
+        dado_inserir.to_csv(f'arquivo/{ctx.guild.id}.csv')
+        arquivo = pd.read_csv(f'arquivo/{ctx.guild.id}.csv')
+        try:
+            arquivo.columns.drop('Unnamed: 0')
+        except:
+            pass
 
-
-    arquivo = pd.read_csv('arquivo/dados.csv')
     arquivo.set_index("ChatID", inplace=True)
 
 
@@ -47,27 +54,31 @@ async def on_message(ctx):
         }
 
         arquivo.rename(index={tamanho: str(ctx.channel.id)}, inplace=True)
-    arquivo.to_csv('arquivo/dados.csv')
+    arquivo.to_csv(f'arquivo/{ctx.guild.id}.csv')
     await bot.process_commands(ctx)
 
 
 @bot.command(aliases=['dado', 'dads', 'estatistica', 'estatistics'])
 @commands.guild_only()
 async def dados(ctx):
-    arquivo = pd.read_csv('arquivo/dados.csv')
+    arquivo = pd.read_csv(f'arquivo/{ctx.guild.id}.csv')
     arquivo.set_index("ChatID", inplace=True)
     fig = arquivo.plot.bar(x='ChatName', y='CountMessage', rot=0).get_figure()
-    fig.savefig('graphics/image.png')
-    picture = discord.File('graphics/image.png')
-    await ctx.send(content=f'{ctx.author.mention}, Aqui está o gráfico, cujo demonstra a intensidade de uso dos chats! ', file=picture)
+    fig.savefig(f'graphics/{ctx.author.id}.png')
+    picture = discord.File(f'graphics/{ctx.author.id}.png')
+    if ctx.guild.owner == ctx.author:
+        var = "\nVocê, **owner do servidor**, pode utilizar o comando `e!deletar_dados`"
+    else:
+        var = ''
+    await ctx.send(content=f'{ctx.author.mention}, Aqui está o gráfico, cujo demonstra a intensidade de uso dos chats! {var}', file=picture)
 
 @bot.event
 @commands.guild_only()
 async def on_guild_channel_delete(channel):
-    arquivo = pd.read_csv('arquivo/dados.csv')
+    arquivo = pd.read_csv(f'arquivo/{channel.guild.id}.csv')
     arquivo.set_index("ChatID", inplace=True)
     arquivo.drop(channel.id, axis=0, inplace=True)
-    arquivo.to_csv('arquivo/dados.csv')
+    arquivo.to_csv(f'arquivo/{channel.guild.id}.csv')
 
 @bot.command(aliases=['drop_data', 'deletar_dados', 'deletar_dado'])
 @commands.guild_only()
@@ -77,10 +88,10 @@ async def drop_dados(ctx):
     else:
         return await ctx.send(f'**Esse comando é exclusivo para o `{ctx.guild.owner}`')
 
-    arquivo = pd.read_csv('arquivo/dados.csv')
+    arquivo = pd.read_csv(f'arquivo/{ctx.guild.id}.csv')
     arquivo.set_index("ChatID", inplace=True)
     arquivo.drop(arquivo.index, inplace=True)
-    arquivo.to_csv('arquivo/dados.csv')
+    arquivo.to_csv(f'arquivo/{ctx.guild.id}.csv')
     await ctx.send('Você apagou todos os dados do comando `e!dados`')
 
 bot.run('Nzg0MzgzMDgxNjY5NzIyMTUz.X8ofqw.PDCOBAbiCKJat96OLFySvq2HRGc')
